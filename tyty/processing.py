3c05de3a8c08156c59d4085d77b78fbfef2be781
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-08-03 21:22:15 +0800

    update function contributors

--- tyty/processing.py
+++ tyty/processing.py
@@ -2,11 +2,21 @@ import pandas as pd
 
 
 def import_data(dataset):
-    """ Imports the provided data as pandas dataframes from the data folder """
+    """
+    Imports the provided data as pandas dataframes from the data folder
+    
+    Contributors:
+    - Daniel
+    """
     return pd.read_csv("data/" + dataset + ".csv")
 
 
 def clean_nan(dataset):
-    """ Removes the empty columns from the provided data """
+    """
+    Removes the empty columns from the provided data
+    
+    Contributors:
+    - Daniel
+    """
     dataset.dropna(how="all", axis=1, inplace=True)
     return dataset
