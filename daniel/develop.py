e08e86ef7f31d61d09873c08386195f992a04caa
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-08-04 20:45:31 +0800

    update agenda

--- daniel/develop.py
+++ daniel/develop.py
@@ -1,21 +1,4 @@
-import pandas as pd
-
 from tyty import processing
-
-
-def import_data(dataset):
-    """
-    Imports the provided data as pandas dataframes from the data folder
-    """
-    return pd.read_csv("data/" + dataset + ".csv")
-
-
-def clean_nan(dataset):
-    """
-    Removes the empty columns from the provided data
-    """
-    dataset.dropna(how="all", axis=1, inplace=True)
-    return dataset
 
 
 data = processing.clean_nan(processing.import_data("data"))
@@ -23,3 +6,5 @@ demo = processing.clean_nan(processing.import_data("demographics"))
 
 print(data.head())
 print(demo.head())
+print(data.columns)
+print(demo.columns)
