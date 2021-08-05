ad7332872ae847cc579d400a800a519deaac073a
Di Yao <DiYao@YaoDis-MacBook-Pro.local>
2021-08-05 11:39:32 +0800

    data read-in and processing- Di

--- di/develop.py
+++ di/develop.py
@@ -1,1 +1,11 @@
-print("hi")
+from data_processing import clean_nan
+from data_processing import import_data
+
+
+data = clean_nan(import_data("data"))
+demo = clean_nan(import_data("demographics"))
+
+print(data.head())
+print(demo.head())
+print(data.columns)
+print(demo.columns)
