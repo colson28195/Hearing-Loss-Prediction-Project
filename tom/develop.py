c1bd44289e9570ab50a148e2457b2a94c7913504
Cheng Nian <nian28195@gmail.com>
2021-08-07 20:56:21 +0800

    started pressure matching

--- tom/develop.py
+++ tom/develop.py
@@ -1,1 +1,25 @@
-print("Hello")
+import pandas as pd
+
+from tyty import pipeline
+import daniel.processing as ps
+
+pd.set_option("display.max_rows", None)
+
+result = pipeline.run_pipeline()
+
+features, target = ps.split_target(result, ["Gender", "EarSide"])
+
+# print(features.head())
+# print(len(result))
+
+PressureMatching = result[["Subject", "AdultAbsorbanceData", "Pressure", "EarSide"]]
+
+PressureMatching["abs"] = (
+    PressureMatching["AdultAbsorbanceData"] - PressureMatching["Pressure"]
+).abs()
+
+# print(PressureMatching.head())
+
+x = PressureMatching.groupby(["Subject", "EarSide"]).min()
+
+print(x)
