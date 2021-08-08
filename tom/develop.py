2e4f22c8258d205562de20f6af79148b970e9192
Cheng Nian <nian28195@gmail.com>
2021-08-08 17:16:11 +0800

    This is my new method which can select rows with minimum abs of the pressure difference, but somehow it neglects the one row that doesnot match(it drops participant 216, left earside)and I cannot figure it out whats wrong with my logic

--- tom/develop.py
+++ tom/develop.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
-from tyty import pipeline
 import daniel.processing as ps
 
 pd.set_option("display.max_rows", None)
 
-result = pipeline.run_pipeline()
+result = ps.run_pipeline()
 
 features, target = ps.split_target(result, ["Gender", "EarSide"])
 
@@ -20,6 +19,26 @@ PressureMatching["abs"] = (
 
 # print(PressureMatching.head())
 
-x = PressureMatching.groupby(["Subject", "EarSide"]).min()
+# x = PressureMatching.groupby(["Subject", "EarSide"]).min()
+
+# print(x)
+
+# def Matching(data):
+# return "match" if data["AdultAbsorbanceData"] == data["Pressure"] else "mismatch"
+
+# result["is_pressure_match"] = result.apply(Matching,axis = 1)
+
+# print(result)
+
+# select_row = result.loc[result["is_pressure_match"] == "match"]
+
+# print(select_row)
+
+x = PressureMatching[
+    PressureMatching["AdultAbsorbanceData"]
+    .sub(PressureMatching["Pressure"])
+    .groupby(PressureMatching["Subject"])
+    .transform(lambda x: x == abs(x).min())
+]
 
 print(x)
