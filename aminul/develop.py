d815c0c80956bb0e30df3962f3a7895c93cc5fe8
aminul <aminul_apece@yahoo.com>
2021-08-13 14:57:08 +0800

    check

--- aminul/develop.py
+++ aminul/develop.py
@@ -2,10 +2,31 @@ import pandas as pd
 
 from tyty import processing
 
-x = processing.import_data("data")
-# print(x.head())
 
-newdf = x.loc[(x.AdultAbsorbanceData == x.Pressure)]
-list = sorted(x)
-print(list)
-print(newdf)
+def run_pipeline():
+    data = processing.import_data("data")
+    data = processing.clean_empty(data)
+    data = processing.remove_columns(
+        data,
+        [
+            "Participant ID",
+            "Protocol",
+            "Created",
+            "Data",
+            "Pressure",
+            "AdultAbsorbanceData",
+            "AgeCategory",
+            "PressureCategory",
+        ],
+    )
+    return data
+
+
+# Explanation for removing these column:
+# participant id can be deleted. it does not required to make any prediction.
+# protocol colunm has no vlaue/ empty column. so it can be deleted.
+# Created column is insignificant. Because it a time when the measurement is taken.
+# data column have similar value in each raw. so it is not significant.
+# Adult absorbance and pressure is required for appropriate raw selection . but as a column , they are not significant.
+# As we are using age directly as numerical value, so AgeCategory just duplicate of that age features.
+# Pressure Category only contain single char data which is "ambient ". so it is not significant.
--- aminul/import.py
+++ aminul/import.py
@@ -1,20 +1,12 @@
 import pandas as pd
 
-from tyty import processing
+from tyty import pipeline
 
-x = processing.import_data("data")
-# participant id can be deleted, although it required to select row for correct pressure.
-x.drop("Participant ID", inplace=True, axis=1)
-# protocol colunm has no vlaue. so it can be deleted
-x.drop("Protocol", inplace=True, axis=1)
-# Created column is insignificant
-x.drop("Created", inplace=True, axis=1)
-# data column have similar value in each raw. so it is not significant.
-x.drop("Data", inplace=True, axis=1)
-# Pressure column also have similar value.
-x.drop("Pressure", inplace=True, axis=1)
-# Adult absorbance and pressure is required for appropriate raw selection . but as a column , they are not significant.
-x.drop("AdultAbsorbanceData", inplace=True, axis=1)
-# drop pressure
-# x.drop('pressure', inplace=True, axis=1)
-print(x.head())
+result = pipeline.run_pipeline()
+
+features, target = pipeline.split_target(result, ["Gender", "EarSide"])
+
+# print(features)
+# Data typedescription:
+data_type = features.describe()
+print(data_type)
