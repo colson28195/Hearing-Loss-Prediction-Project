36fbf7798ea7c7f4765219f32774ffa0d62a32ec
aminul <aminul_apece@yahoo.com>
2021-09-01 20:24:56 +0800

    i got accuracy 84% for training and 85% for test

--- aminul/develop.py
+++ aminul/develop.py
@@ -7,14 +7,19 @@ from tyty import pipeline
 
 
 train_data, test_data, train_labels, test_labels = pipeline.full_pipeline(
-    feature_columns=["Gender", "EarSide", "Age"]
+    feature_columns=["Gender", "Age"]
 )
 
-model = LogisticRegression()
+model = LogisticRegression(penalty="l1", solver="liblinear")
 
-train_pred, test_pred = pipeline.modelling_pipeline(
+train_pred, test_pred, trained_model = pipeline.modelling_pipeline(
     model, train_data, train_labels, test_data
 )
 
 beluga.metrics.summary(train_labels, train_pred, conditions=True)
 beluga.metrics.summary(test_labels, test_pred, conditions=True)
+
+importance = model.coef_[0]
+# summarize feature importance
+for i, v in enumerate(importance):
+    print("Feature: %0d, Score: %.5f" % (i, v))
