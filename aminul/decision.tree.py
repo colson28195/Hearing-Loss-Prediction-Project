a96b7761f26756dbc31af9fea34e68c5d285705c
aminul <aminul_apece@yahoo.com>
2021-09-23 13:31:34 +0800

    i have decision tree with deafult parametars. with training it gives accuracy 100% and with test set accuracy is 76%

--- aminul/decision.tree.py
+++ aminul/decision.tree.py
@@ -5,13 +5,9 @@ import beluga
 from tyty import pipeline
 
 
-train_data, test_data, train_labels, test_labels, features = pipeline.full_pipeline(
+train_data, test_data, train_labels, test_labels = pipeline.full_pipeline(
     feature_columns=["Gender", "EarSide", "Age"]
 )
-
-# train_data = train_data.drop(["f(250)", "f(400)", "f(2500)"], axis=1)
-# test_data = test_data.drop(["f(250)", "f(400)", "f(2500)"], axis=1)
-
 
 model = LogisticRegression(penalty="l1", solver="liblinear")
 
@@ -21,10 +17,3 @@ train_pred, test_pred, trained_model = pipeline.modelling_pipeline(
 
 beluga.metrics.summary(train_labels, train_pred, conditions=True)
 beluga.metrics.summary(test_labels, test_pred, conditions=True)
-
-# importance = model.coef_[0]
-# feature_importance = abs(model.coef_[0])
-# feature_importance = 100.0 * (feature_importance / feature_importance.max())
-
-
-# print(feature_importance )
--- aminul/dtree.py
+++ aminul/dtree.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
 import beluga
 from tyty import pipeline
 
@@ -9,11 +11,7 @@ train_data, test_data, train_labels, test_labels, features = pipeline.full_pipel
     feature_columns=["Gender", "EarSide", "Age"]
 )
 
-# train_data = train_data.drop(["f(250)", "f(400)", "f(2500)"], axis=1)
-# test_data = test_data.drop(["f(250)", "f(400)", "f(2500)"], axis=1)
-
-
-model = LogisticRegression(penalty="l1", solver="liblinear")
+model = DecisionTreeClassifier(random_state=0)
 
 train_pred, test_pred, trained_model = pipeline.modelling_pipeline(
     model, train_data, train_labels, test_data
@@ -21,10 +19,3 @@ train_pred, test_pred, trained_model = pipeline.modelling_pipeline(
 
 beluga.metrics.summary(train_labels, train_pred, conditions=True)
 beluga.metrics.summary(test_labels, test_pred, conditions=True)
-
-# importance = model.coef_[0]
-# feature_importance = abs(model.coef_[0])
-# feature_importance = 100.0 * (feature_importance / feature_importance.max())
-
-
-# print(feature_importance )
