744b2f161b0cc14a8fe1207955b11191091e106d
aminul <aminul_apece@yahoo.com>
2021-10-12 20:03:26 +0800

    i have added some image file and made change in tunning.py

--- aminul/tunning.py
+++ aminul/tunning.py
@@ -18,12 +18,11 @@ cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 space = dict()
 space["solver"] = ["newton-cg", "lbfgs", "liblinear"]
 space["penalty"] = ["none", "l1", "l2", "elasticnet"]
-space["C"] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
 
 # define search
 search = GridSearchCV(model, space, scoring="accuracy", n_jobs=-1, cv=cv)
 # execute search
-result = search.fit(train_data, train_labels)
+result = search.fit(test_data, test_labels)
 # summarize result
 print("Best Score: %s" % result.best_score_)
 print("Best Hyperparameters: %s" % result.best_params_)
